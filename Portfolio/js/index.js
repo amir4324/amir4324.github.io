4d366e16ba37f2a31639122d8ba9ff9c69d6549b
amir4324 <amir6565@gmail.com>
2020-03-04 08:32:10 +0300

    portfolio update

---
+++ Portfolio/js/index.js
@@ -0,0 +1,15 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navlinks = document.querySelectorAll('.nav__link');
+
+
+navToggle.addEventListener('click', () => {
+
+    document.body.classList.toggle('nav-open');
+
+});
+
+navlinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
